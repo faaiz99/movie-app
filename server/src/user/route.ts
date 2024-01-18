0ab465bca8d25d5979684849fc3eeba28b13ff18
faaiz99 <faaizaslam75@live.com>
2024-01-18 23:53:45 +0500

    refresh token deleted from server

--- server/src/user/route.ts
+++ server/src/user/route.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { login, register, refreshToken, deleteUser } from "./controller";
+import { login, register, deleteUser } from "./controller";
 import { checkSchema } from "express-validator";
 
 const router = Router();
