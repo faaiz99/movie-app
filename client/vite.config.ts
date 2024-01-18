c859cc822d94803852fcb5cceeecc94b07dfcc2c
faaiz99 <faaizaslam75@live.com>
2024-01-18 23:52:20 +0500

    dependencies added, assets added, configs updated

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import tsconfigPaths from "vite-tsconfig-paths";
+import { fileURLToPath } from "node:url";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()],
+  resolve: {
+    alias: [
+      {
+        find: "@",
+        replacement: fileURLToPath(new URL("./src", import.meta.url)),
+      },
+    ],
+  },
 });
