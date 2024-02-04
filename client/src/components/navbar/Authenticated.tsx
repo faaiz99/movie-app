6ba3ecb246b70e46d595a629d022ce9405c0edb9
faaiz99 <faaizaslam75@live.com>
2024-02-05 03:17:36 +0500

    Refactor - gitignore updated

--- client/src/components/navbar/Authenticated.tsx
+++ client/src/components/navbar/Authenticated.tsx
@@ -1,7 +1,6 @@
 import { Dropdown } from "flowbite-react";
 import { useNavigate } from "react-router-dom";
 import { useAuthStore } from "../../store/store";
-import { Avatar } from "flowbite-react";
 
 export const Authenticated = () => {
   const { email, firstName, lastName } = useAuthStore((state) => state.session);
@@ -20,21 +19,15 @@ export const Authenticated = () => {
       arrowIcon={false}
       inline
       label={
-        <Avatar
-          alt="User settings"
-          img="https://flowbite.com/docs/images/people/profile-picture-5.jpg"
-          rounded
-        />
+        <div className="relative w-10 h-10 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600">
+          <svg className="absolute w-12 h-12 text-gray-400 -left-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clip-rule="evenodd"></path></svg>
+        </div>
       }
     >
       <Dropdown.Header>
         <span className="block text-sm">{firstName + " " + lastName}</span>
         <span className="block truncate text-sm font-medium">{email}</span>
       </Dropdown.Header>
-      {/* <Dropdown.Item>Movies</Dropdown.Item> */}
-      {/* <Dropdown.Item>Modify Movies</Dropdown.Item>
-      <Dropdown.Item>Delete Account</Dropdown.Item> */}
-      <Dropdown.Divider />
       <Dropdown.Item onClick={HandleLogout}>Logout</Dropdown.Item>
     </Dropdown>
   );
--- client/src/components/navbar/UnAuthenticated.tsx
+++ client/src/components/navbar/UnAuthenticated.tsx
@@ -1,21 +1,17 @@
 import { Dropdown } from "flowbite-react";
 import { useNavigate } from "react-router-dom";
-import { Avatar } from "flowbite-react";
 
 export const UnAuthenticated = () => {
   const navigate = useNavigate();
-  /** CHECK IF LABEL CAN BE REPLACED */
   return (
     <Dropdown
       data-testid="unauthenticated"
       arrowIcon={false}
       inline
       label={
-        <Avatar
-          alt="User settings"
-          img="https://flowbite.com/docs/images/people/profile-picture-5.jpg"
-          rounded
-        />
+        <div className="relative w-10 h-10 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600">
+          <svg className="absolute w-12 h-12 text-gray-400 -left-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clip-rule="evenodd"></path></svg>
+        </div>
       }
     >
       <Dropdown.Item onClick={() => navigate("/login")}>Login</Dropdown.Item>
