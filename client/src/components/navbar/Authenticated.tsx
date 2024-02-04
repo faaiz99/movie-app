6235491ff9acada1e56bfd5692f9cd316f5c78e3
faaiz99 <faaizaslam75@live.com>
2024-02-05 03:19:34 +0500

    Bug - svg error fixed

--- client/src/components/navbar/Authenticated.tsx
+++ client/src/components/navbar/Authenticated.tsx
@@ -19,8 +19,19 @@ export const Authenticated = () => {
       arrowIcon={false}
       inline
       label={
-        <div className="relative w-10 h-10 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600">
-          <svg className="absolute w-12 h-12 text-gray-400 -left-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clip-rule="evenodd"></path></svg>
+        <div className="relative h-10 w-10 overflow-hidden rounded-full bg-gray-100 dark:bg-gray-600">
+          <svg
+            className="absolute -left-1 h-12 w-12 text-gray-400"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fillRule="evenodd"
+              d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
+              clipRule="evenodd"
+            ></path>
+          </svg>
         </div>
       }
     >
